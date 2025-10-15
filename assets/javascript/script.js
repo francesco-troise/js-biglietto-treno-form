747c08a96d17c3d721cc7ffd8670890ae40f23d2
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-15 15:56:32 +0200

    Add DOM node selection

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form")
+//Selezione del form
+const full_name_field = document.getElementById("input_full_name")
+//Selezione del nome
+const km_field = document.getElementById("input-km")
+//Selezione dei km
+const age_field = document.getElementById("select-age")
+//Selezione dell'età
+
+const td_nome = document.getElementById("td-nome")
+const td_offerta = document.getElementById("td-offerta")
+const td_carrozza = document.getElementById("td-carrozza")
+const td_codice = document.getElementById("td-codice")
+const td_costo = document.getElementById("td-costo")
+//Selezione dei nodi appartenenti alla tabella di riepilogo del biglietto
+
